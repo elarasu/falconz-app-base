f52348bfc1818f3b1a2f6d933e5ce186bb286b26
Eyal Arasu <elarasu@gmail.com>
2021-10-18 02:12:19 -0700

    fix: expose Service api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Log from './lib/log';
 import { Logger } from './lib/logger';
+import Service from './lib/service';
 import * as Utils from './lib/utils';
 
-export { Log, Logger, Utils };
+export { Log, Logger, Utils, Service };
